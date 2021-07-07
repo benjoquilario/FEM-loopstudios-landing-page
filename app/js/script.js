76b43503173c1b4b5458ea04e50fa7b97f81ea22
iambenjo <82529126+benjoquilario@users.noreply.github.com>
2021-07-07 22:12:02 +0800

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".btn--menu");
+const icon = document.querySelector(".hamburger");
+const navLinks = document.querySelectorAll(".navbar__links");
+const navbarMenu = document.querySelector(".navbar__menu");
+const body = document.querySelector("body");
+const active = () => {
+  navbarMenu.classList.toggle("active");
+  body.classList.toggle("disabled-scroll");
+  if (navbarMenu.classList.contains("active")) {
+    icon.src = "/images/icon-close.svg";
+    button.setAttribute("aria-expanded", "false");
+  } else {
+    icon.src = "/images/icon-hamrburger.svg";
+    button.setAttribute("aria-expanded", "true");
+  }
+};
+button.addEventListener("click", active);
